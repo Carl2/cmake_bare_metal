c76011399441c16596c8d7bfa7b81c01fdaf2974
Calle Olsen <co@cellink.com>
2022-06-20 17:32:23 +0200

    {WIP} Command Parsing

---
+++ src/uart_bin_parser/cpp/command_parser.hpp
@@ -0,0 +1,18 @@
+#pragma once
+#include <tuple>
+
+namespace msg
+{
+
+using CommandFnVariants =
+
+    using CmdMapping = std::tuple<CommandEnum, CommandFn>;
+// Mapping betweeen command and a function
+
+struct GuppiCmdProtocol
+{
+
+    type name(args) const;
+};
+
+}  // namespace msg
